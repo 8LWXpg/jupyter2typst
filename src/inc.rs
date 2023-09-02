6ff7711569341af1cf4e94ef94bb9f3d6c3b1a0b
8LWXpg <v3ak6xhthk@gmail.com>
2023-09-02 13:37:34 +0800

    add test

--- src/inc.rs
+++ src/inc.rs
@@ -5,13 +5,15 @@ pub fn ipynb_parse(json: Value) -> String {
 
     for cell in json["cells"].as_array().unwrap() {
         let cell_type = cell["cell_type"].as_str().unwrap();
-        let source = cell["source"].as_str().unwrap();
         match cell_type {
             "markdown" => {
-                output.push_str(&md_to_typst(source));
+                output.push_str(&md_to_typst(cell["source"].as_str().unwrap()));
             }
             "code" => {
-                output.push_str(&code_parse(source));
+                output.push_str(&code_parse(
+                    cell["source"].as_str().unwrap(),
+                    cell["execution_count"].as_str().unwrap(),
+                ));
                 let outputs = cell["outputs"].as_array().unwrap();
                 output.push_str(&code_output_parse(outputs));
             }
@@ -30,7 +32,7 @@ fn md_to_typst(md: &str) -> String {
     output
 }
 
-fn code_parse(code: &str) -> String {
+fn code_parse(code: &str, count: &str) -> String {
     let mut output = String::new();
 
     // TODO https://nbformat.readthedocs.io/en/latest/format_description.html#code-cells
