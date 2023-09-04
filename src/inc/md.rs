dc94cc40aee0a39cea6f305e15e4a6af8497cc94
8LWXpg <v3ak6xhthk@gmail.com>
2023-09-04 21:23:31 +0800

    ast

--- src/inc/md.rs
+++ src/inc/md.rs
@@ -1,4 +1,13 @@
+use std::{fs::File, io::Write};
+
+use markdown::{to_mdast, ParseOptions};
+
 pub fn md_to_typst(md: Vec<&str>) -> String {
-    md.join("")
-    // TODO https://github.com/kevinmehall/rust-peg
+    // TODO https://github.com/wooorm/markdown-rs
+    let tree = to_mdast(md.join("").as_str(), &ParseOptions::default()).unwrap();
+    // write tree to debug file
+    let mut file = File::create("debug.txt").unwrap();
+    file.write_all(format!("{:#?}", tree).as_bytes()).unwrap();
+
+    String::from("TODO")
 }
