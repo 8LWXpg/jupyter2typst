1de23ca2febea1f441132aebfc7476e104d87597
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-07-16 16:22:28 +0800

    -o max

--- template/template.ps1
+++ template/template.ps1
@@ -4,6 +4,6 @@ cd $PSScriptRoot
 ls *.typ -e base.typ | % {
     "#import `"$($_.Name)`": *", (cat ./base.typ) | typst c - "./out/$($_.BaseName).png"
 }
-oxipng ./out/*.png
+oxipng ./out/*.png -o max
 
 popd
